f1e8eabb9995a164cf604b4366b3bbfb2492cddf
rkarshiev <rkarshiev@anresh.ru>
2019-12-27 19:43:09 +0300

    added postcss, babel, etc

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,23 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.scss$/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader
                     },
                     'css-loader',
+                    'postcss-loader',
                     'sass-loader'
                 ]
             }
